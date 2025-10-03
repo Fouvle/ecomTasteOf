e5ff55dd63dafa9ebed9a1546ae6efd07a3be105
Fouvle <132784224+Fouvle@users.noreply.github.com>
2025-10-03 23:39:28 +0000

    Update dbforlab.sql

--- db/dbforlab.sql
+++ db/dbforlab.sql
@@ -9,7 +9,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE DATABASE IF NOT EXISTS ` ecommerce_2025A_nana_nkrumah`;
 USE `ecommerce_2025A_nana_nkrumah`;
 
 -- --------------------------------------------------------
