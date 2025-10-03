8aa6e2b7707d324fdf007a3db044739a1440c17c
Fouvle <132784224+Fouvle@users.noreply.github.com>
2025-10-03 23:58:08 +0000

    e

--- settings/db_cred_local.php
+++ settings/db_cred_local.php
@@ -2,7 +2,6 @@
 //db credentials
 
 define('SERVER', 'localhost');
-define('USERNAME', 'nana.nkrumah');
-define('PASSWORD', 'nkrucom187');
+define('USERNAME', 'root');
+define('PASSWORD', '');
 define('DATABASE', 'ecommerce_2025A_nana_nkrumah');
-?>
