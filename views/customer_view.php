43db2ce930383f715025a9cc654fa796b4af3c4a
Fouvle <132784224+Fouvle@users.noreply.github.com>
2025-10-28 11:27:52 +0000

    Update customer_view.php

--- views/customer_view.php
+++ views/customer_view.php
@@ -122,6 +122,6 @@ if (isAdmin()) {
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="customerview.js"></script>
+    <script src="../js/customerview.js"></script>
 </body>
 </html>
